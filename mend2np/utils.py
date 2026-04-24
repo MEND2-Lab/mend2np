322801730e60cf1e3d8f633ffc66f8d88a5dd55d
Matthew Thompson <mbthompson14@gmail.com>
2026-04-24 16:03:25 -0400

    handler specific log levels

--- mend2np/utils.py
+++ mend2np/utils.py
@@ -11,13 +11,15 @@ from ast import literal_eval
 
 def setup_logger(name:str='root', out:str='out', level:int|str=20):
     logger = logging.getLogger(name)
-    logger.setLevel(level)
+    #logger.setLevel(level)
     datetime_string = datetime.now().strftime('%Y%m%d_%H%M%S')
     formatter = logging.Formatter(fmt='%(asctime)s : %(levelname)s : %(module)s : %(message)s')
     stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(level)
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
     file_handler = logging.FileHandler(os.path.join(out,f'log_{datetime_string}.log'),mode='w')
+    file_handler.setLevel(level)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     return logger
